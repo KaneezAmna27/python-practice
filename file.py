f189f2dd10f9da275885aad0273c876e5e6cbcf6
AmmanRizwan <ammanrizwan00007@gmail.com>
2024-11-27 18:02:48 +0530

    add the task

--- file.py
+++ file.py
@@ -1,1 +1,15 @@
-print("hello, world")
+"""
+Display this in Terminal:
+
+
+-------------------------
+1. Add the List
+2. Update the List
+3. Delete the List
+4. Traverse the List
+5. Exit
+-------------------------
+
+"""
+
+# Use the print() function to print this message
