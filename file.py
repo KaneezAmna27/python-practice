8bc92aa8e003ace7bff084fbdb3d42f8085ec37a
kaneezAmna27 <aminakaneez95@gmail.com>
2024-11-28 18:48:58 +0530

    solved

--- file.py
+++ file.py
@@ -1,7 +1,10 @@
-"""
-Q2. WAP to print 1 to 10 using while loop
-"""
+# """
+# Q2. WAP to print 1 to 10 using while loopgit
+# """
 
-"""
-Q3. WAP to print 1 to 10 using for loop
-"""
+# """
+# Q3. WAP to print 1 to 10 using for loop
+# """
+# 
+for i in range(1,11):
+    print(i)
