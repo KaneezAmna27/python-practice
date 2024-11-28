523fdb1a491f1f63f3392415b8ab9a5c6df4453f
kaneezAmna27 <aminakaneez95@gmail.com>
2024-11-28 18:07:39 +0530

    second commit

--- file.py
+++ file.py
@@ -1,15 +1,21 @@
-"""
-Display this in Terminal:
+# """
+# Display this in Terminal:
 
 
--------------------------
-1. Add the List
-2. Update the List
-3. Delete the List
-4. Traverse the List
-5. Exit
--------------------------
+# -------------------------
+# 1. Add the List
+# 2. Update the List
+# 3. Delete the List
+# 4. Traverse the List
+# 5. Exit
+# -------------------------
 
-"""
+# """
 
 # Use the print() function to print this message
+list = [2,7,8]
+list.append(9)
+print(list)
+# list = [4,7,8,9]
+# for i in range (0,4,1):
+#     print(list)
